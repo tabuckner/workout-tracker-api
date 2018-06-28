065e5ff8fe632356243c7b591cc023cfeaa6ff86
Taylor Buckner <tbuckner@theappraisallane.com>
2018-06-27 19:50:07 -0500

    Fixed timestamp issue in schema

--- src/models/Exercise.ts
+++ src/models/Exercise.ts
@@ -1,12 +1,10 @@
 import { Schema, model} from 'mongoose';
 
 let exerciseSchema: Schema = new Schema({
-  createdAt: Date,
-  updatedAt: Date,
   name: { type: String, required: true },
   sets: { type: Number, required: true },
   reps: { type: Number, required: true },
   weight: { type: Number, required: true },
-});
+}, { timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' } });
 
 export default model('Exercise', exerciseSchema);
--- src/models/Routine.ts
+++ src/models/Routine.ts
@@ -1,12 +1,10 @@
 import { Schema, model} from 'mongoose';
 
 let routineSchema: Schema = new Schema({
-  createdAt: Date,
-  updatedAt: Date,
   name: { type: String, required: true },
   exercises: [
     { type: Schema.Types.ObjectId, ref: 'Exercise' },
-  ]
-});
+  ],
+}, { timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' } });
 
 export default model('Routine', routineSchema);
